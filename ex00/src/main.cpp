ec493387e775172e332681a2d5db06d0e389e001
fcretin <fcretin@student.42lyon.fr>
2025-06-26 14:58:57 +0200

    ex00 done ? ex01 started

---
+++ ex00/src/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include "easyfind.hpp"
+
+int main()
+{
+	std::vector<int> vcontainer;
+
+	vcontainer.push_back(10);
+	vcontainer.push_back(9);
+	vcontainer.push_back(8);
+	vcontainer.push_back(7);
+	vcontainer.push_back(6);
+	vcontainer.push_back(5);
+	vcontainer.push_back(4);
+	vcontainer.push_back(3);
+	vcontainer.push_back(2);
+	vcontainer.push_back(1);
+	vcontainer.push_back(0);
+	std::vector<int>::iterator	it = vcontainer.end();
+
+	try
+	{
+		it = easyfind(vcontainer, 11);
+	}
+	catch (std::exception &e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+	std::cout << *it << std::endl;
+	return 0;
+}
---
+++ ex01/inc/Span.hpp
@@ -0,0 +1,88 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Span.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fcretin <fcretin@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/26 14:27:31 by fcretin           #+#    #+#             */
+/*   Updated: 2025/06/26 14:58:32 by fcretin          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SPAN_HPP
+#define SPAN_HPP
+
+#include <algorithm>
+#include <iterator>
+#include <iostream>
+#include <exception>
+
+#define RESET "\033[0m"
+#define RED "\033[31m"
+#define GREEN  "\033[32m"
+#define BLUE  "\033[34m"
+#define YELLOW "\033[33m"
+
+// class NotFoundException: public std::exception
+// {
+// 	public :
+// 		virtual const char* what() const throw() {
+// 			return ("Not found.");
+// 		}
+// };
+
+
+// template <typename T>
+// typename T::iterator	easyfind(T &container, int value)
+// {
+// 	typename T::iterator it;
+
+// 	it = std::find(container.begin(), container.end(), value);
+// 	if (it == container.end())
+// 		throw (NotFoundException());
+// 	return (it);
+// }
+
+class StockCapped : std::exception
+{
+	public :
+		virtual const char *what() const throw(){
+			return  ("Stock Max Reached.");
+		}
+}
+
+class Span
+{
+	private :
+	// {
+
+		/* Canonical */
+		Span( void );
+		Span(const Span &other);
+		Span &operator=(const Span &other);
+		~Span( void );
+
+		int	_MaxStock;
+		int	_ActualStock;
+
+
+	// }
+	public :
+	// {
+
+		Span( unsigned int &N );
+
+		void	addNumber();
+		void	shortestSpan();
+		void	longestSpan();
+
+		/* bonus */
+		void	randomFill();
+		void	print();
+		void	popNumber;
+	// }
+};
+
+
+ #endif
